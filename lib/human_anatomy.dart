e936da625d47497a693ed28330122c6bbd10394d
Mico <Miloloza>
2020-07-01 22:13:05 +0200

    body alignment improvements

--- lib/human_anatomy.dart
+++ lib/human_anatomy.dart
@@ -40,55 +40,50 @@ class _HumanAnatomyState extends State<HumanAnatomy> {
             bodyPart("assets/left_eye.svg", "LeftEye", 22.0, 20.0, 0.0, 10.0),
             bodyPart("assets/right_eye.svg", "rightEye", 22.0, 0.0, 20.0, 10.0),
             bodyPart("assets/nose.svg", "Nose", 25.0, 0.0, 0.0, 20.0),
-            bodyPart("assets/mouth.svg", "Mouth", 48.0, 0.0, 0.0, 10.0),
+            // bodyPart("assets/mouth.svg", "Mouth", 48.0, 0.0, 0.0, 10.0),
             bodyPart("assets/neck.svg", "Neck", 58.0, 0.0, 0.0, 40.0),
-            bodyPart("assets/chest_right.svg", "ChestRight", 93.0, 0.0, 0.0, 95.0),
-            bodyPart("assets/chest_left.svg", "ChestLeft", 93.0, 0.0, 0.0, 95.0),
+
+            bodyPart("assets/chest_right.svg", "ChestRight", 93.0, 58.0, 0.0, 95.0),
+            bodyPart("assets/chest_left.svg", "ChestLeft", 93.0, 0.0, 50.0, 95.0),
+
             bodyPart("assets/abdomin.svg", "Abdomin", 175.0, 0.0, 0.0, 65.0),
+
             bodyPart("assets/pelvis.svg", "Pelvis", 225.0, 0.0, 0.0, 50.0),
-            bodyPart("assets/publs.svg", "Publs", 275.0, 0.0, 0.0, 15.0),
-            bodyPart("assets/left_soulder.svg", "leftSoulder", 90.0, 105.0, 0.0,
-                50.0),
-            bodyPart("assets/right_soulder.svg", "rightSoulder", 89.0, 0.0,
-                110.0, 50.0),
-            bodyPart("assets/left_arm.svg", "leftArm", 120.0, 145.0, 0.0, 70.0),
-            bodyPart(
-                "assets/right_arm.svg", "rightArm", 118.0, 0.0, 142.0, 70.0),
-            bodyPart(
-                "assets/left_elbow.svg", "leftElbow", 172.0, 172.0, 0.0, 39.0),
-            bodyPart("assets/right_elbow.svg", "rightElbow", 170.0, 0.0, 170.0,
-                40.0),
-            bodyPart("assets/left_forearm.svg", "leftForearm", 195.0, 190.0,
-                0.0, 54.0),
-            bodyPart("assets/right_forearm.svg", "rightForearm", 195.0, 0.0,
-                190.0, 54.0),
-            bodyPart(
-                "assets/left_wrist.svg", "leftWrist", 238.0, 220.0, 0.0, 23.0),
-            bodyPart("assets/right_wrist.svg", "rightWrist", 238.0, 0.0, 220.0,
-                23.0),
-            bodyPart(
-                "assets/left_hand.svg", "leftHand", 250.0, 250.0, 0.0, 60.0),
-            bodyPart(
-                "assets/right_hand.svg", "rightHand", 250.0, 0.0, 250.0, 60.0),
-            bodyPart(
-                "assets/left_thigh.svg", "leftThigh", 242.0, 63.0, 0.0, 138.0),
-            bodyPart("assets/right_thigh.svg", "rightThigh", 242.0, 0.0, 63.0,
-                138.0),
-            bodyPart(
-                "assets/left_knee.svg", "leftKnee", 360.0, 68.0, 0.0, 48.0),
-            bodyPart(
-                "assets/right_knee.svg", "rightKnee", 360.0, 0.0, 68.0, 48.0),
-            bodyPart("assets/left_leg.svg", "leftLeg", 395.0, 64.0, 0.0, 105.0),
-            bodyPart(
-                "assets/right_leg.svg", "rightLeg", 393.0, 0.0, 65.0, 106.0),
-            bodyPart(
-                "assets/left_ankle.svg", "leftAnkle", 495.0, 64.0, 0.0, 25.0),
-            bodyPart(
-                "assets/right_ankle.svg", "rightAnkle", 493.0, 0.0, 68.0, 25.0),
-            bodyPart(
-                "assets/left_foot.svg", "leftFoot", 510.0, 80.0, 0.0, 60.0),
-            bodyPart(
-                "assets/right_foot.svg", "rightFoot", 508.0, 0.0, 83.0, 56.0),
+
+            bodyPart("assets/publs.svg", "Publs", 273.0, 0.0, 0.0, 15.0),
+
+            bodyPart("assets/left_soulder.svg", "leftSoulder", 90.0, 112.0, 0.0, 50.0),
+            bodyPart("assets/right_soulder.svg", "rightSoulder", 89.0, 0.0, 105.0, 50.0),
+
+            bodyPart("assets/left_arm.svg", "leftArm", 120.0, 149.0, 0.0, 70.0),
+            bodyPart("assets/right_arm.svg", "rightArm", 118.0, 0.0, 138.0, 70.0),
+
+            bodyPart("assets/left_elbow.svg", "leftElbow", 173.0, 175.0, 0.0, 39.0),
+            bodyPart("assets/right_elbow.svg", "rightElbow", 170.0, 0.0, 164.0, 40.0),
+
+            bodyPart("assets/left_forearm.svg", "leftForearm", 198.0, 192.0, 0.0, 54.0),
+            bodyPart("assets/right_forearm.svg", "rightForearm", 196.0, 0.0, 182.0, 54.0),
+
+            bodyPart("assets/left_wrist.svg", "leftWrist", 238.0, 218.0, 0.0, 23.0),
+            bodyPart("assets/right_wrist.svg", "rightWrist", 238.0, 0.0, 210.0, 23.0),
+
+            bodyPart( "assets/left_hand.svg", "leftHand", 250.0, 250.0, 0.0, 60.0),
+            bodyPart("assets/right_hand.svg", "rightHand", 250.0, 0.0, 240.0, 60.0),
+
+            bodyPart("assets/left_thigh.svg", "leftThigh", 240.0, 65.0, 0.0, 138.0),
+            bodyPart("assets/right_thigh.svg", "rightThigh", 240.0, 0.0, 62.0, 138.0),
+
+            bodyPart("assets/left_knee.svg", "leftKnee", 360.0, 70.0, 0.0, 48.0),
+            bodyPart("assets/right_knee.svg", "rightKnee", 360.0, 0.0, 68.0, 48.0),
+
+            bodyPart("assets/left_leg.svg", "leftLeg", 395.0, 66.0, 0.0, 106.0),
+            bodyPart("assets/right_leg.svg", "rightLeg", 395.0, 0.0, 67.0, 106.0),
+
+            bodyPart("assets/left_ankle.svg", "leftAnkle", 495.0, 68.0, 0.0, 25.0),
+            bodyPart("assets/right_ankle.svg", "rightAnkle", 495.0, 0.0, 70.0, 25.0),
+
+            bodyPart("assets/left_foot.svg", "leftFoot", 510.0, 86.0, 0.0, 60.0),
+            bodyPart("assets/right_foot.svg", "rightFoot", 510.0, 0.0, 87.0, 60.0),
           ],
         ),
       ),
